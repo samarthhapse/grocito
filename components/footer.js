90c06aef8bdd54fa0785fc0648980bf5ffe99f51
Samarth <samarthhapse97@gmail.com>
2024-02-19 14:55:15 +0530

    grocito

---
+++ components/footer.js
@@ -0,0 +1,77 @@
+function footer(){
+    return ` 
+    <div id="center">
+    <div id="footer_head">
+    <div class="head">
+        <h2>We deliver everything in 10 minutes</h2>
+        <p>blinkit (formerly Grofers) is India's fastest online shopping platform. Get fresh groceries and much more delivered online instantly.</p>
+    </div>
+    <div class="head">
+        <h2> Single app for all your daily needs</h2>
+        <p>Order thousands of products at just a tap: milk, eggs, bread, cooking oil, ghee, atta, rice, fresh fruits and vegetables, spices, fresh meat, seafood, frozen veg and non-veg food, chocolates, chips, biscuits, maggi, cold
+            drinks, shampoos, soaps, body wash, pet food, diapers, electronics, 
+            sanitizers, masks, sanitary napkins, health products, hygiene products, organic products and gourmet products, and much more from your neighbourhood stores.
+         </p>
+    </div>
+    <div class="head">
+        <h2>Available in following cities</h2>
+        <p>The delivery service is operational in Agra, Ahmedabad, Aligarh, Allahabad, Bengaluru, Bhiwadi, Bijnor, Chandigarh, Chennai, Delhi, Durgapur, Faridabad, Gauriganj, Gonda, Gurugram, Guwahati, Hisar, Hyderabad, Jaipur,<br> 
+            Jamshedpur, Kanpur, Kolkata, Kota, Lucknow, Mathura, Meerut, Mohali, Moradabad, Mumbai, Noida, Panchkula, Panipat, Pune, Raebareli, Rohtak, Sonipat, Vadodara, Zirakpur
+        </p>
+    </div>
+</div>
+
+<div id="right">
+    <div class="first">
+        <div class="title">
+            <p>company</p>
+        </div>
+        <div class="right_list">
+          <ul>
+              <a href=""><li>About</li></a>
+              <a href=""><li>career</li></a>
+              <a href=""><li>blog</li></a>
+              <a href=""><li>press</li></a>
+              <a href=""><li>lead</li></a>
+              <a href=""><li>value</li></a>
+          </ul>
+        </div>
+    </div>
+    <div class="second">
+        <div class="title">
+            <p>For consumers</p>
+        </div>
+        <div class="right_list">
+          <ul>
+              <a href=""><li>Privacy</li></a>
+              <a href=""><li>Terms</li></a>
+              <a href=""><li>Faq</li></a>
+              <a href=""><li>secuirity</li></a>
+              <a href=""><li>mobile</li></a>
+              <a href=""><li>contract</li></a>
+          </ul>
+        </div>
+    </div>
+    <div class="third">
+        <div class="title">
+            <p>For Partner</p>
+        </div>
+        <div class="right_list">
+          <ul>
+              <a href=""><li>Partner</li></a>
+              <a href=""><li>Express</li></a>
+              <a href=""><li>Local</li></a>
+              <a href=""><li>spotlight</li></a>
+              <a href=""><li>warehouse</li></a>
+              <a href=""><li>deliver</li></a>
+          </ul>
+        </div>
+    </div>
+</div>
+</div>
+<div id="policy">
+ © SAMARTH HAPSE - Project for GROCITO . All rights reserved.
+</div>
+</div> `
+}
+export default footer;
